5a20646b843ccd51045a54738dedf80a8b86d7c6
Srinivasan Madaswamy <srinivasanmadaswamy@Srinivasans-iMac.local>
2015-10-16 17:28:25 -0700

    change alert function to parseInt.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+ $(document).ready(function() {
+ var num = parseInt(prompt("What number would you like me to ping-pong up to?"));
+  for  (i = 1; i <= num; i += 1) {
+
+   if (i % 15 === 0) {
+     $("#list").append("<li>" + "ping-pong" + "</li>");
+    }
+
+    else if (i % 3 === 0) {
+      $("#list").append("<li>" + "ping" + "</li>");
+    }
+
+   else if (i % 5 === 0) {
+     $("#list").append("<li>" + "pong" + "</li>");
+    }
+   else {
+     $("#list").append("<li>" + i + "</li>");
+    }
+   }
+    event.preventDefault();
+
+ });
