eb9f8124ec20ece2343092bffbc3da20b12e8534
Srinivasan Madaswamy <srinivasanmadaswamy@Srinivasans-iMac.local>
2015-10-16 20:47:33 -0700

    Add styles.css file.

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,19 @@
 
    if (i % 15 === 0) {
      $("#list").append("<li>" + "ping-pong" + "</li>");
-    }
+   }
 
-    else if (i % 3 === 0) {
-      $("#list").append("<li>" + "ping" + "</li>");
-    }
+   else if (i % 3 === 0) {
+     $("#list").append("<li>" + "ping" + "</li>");
+   }
 
    else if (i % 5 === 0) {
      $("#list").append("<li>" + "pong" + "</li>");
-    }
+   }
    else {
      $("#list").append("<li>" + i + "</li>");
-    }
    }
-    event.preventDefault();
+  }
+  event.preventDefault();
 
- });
+  });
